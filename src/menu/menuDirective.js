bd083744249c573b2f77a68c1f4e0b0de03426b3
shwetapurushe <shwetapurushe@gmail.com>
2015-05-15 07:47:13 +0530

    better menu items (circle position better)

--- src/menu/menuDirective.js
+++ src/menu/menuDirective.js
@@ -21,19 +21,16 @@ myapp.directive('fancyMenu', function(){
 
             },
 
-            link : function(scope, elem, attrs){
+            link : function(scope){
 
                 /*
                  * fancy menu code
                  *
                  * */
 
-                var w = 500;
-                var h = 500;
-
-                var aboutC = { "x": 0, "y": 0, "radius": 80, "color" : "#33CCCC", endX : 180, endY: 380, path : '/about', text : "About"};
-                var publicationsC = { "x": 0, "y": 0, "radius": 80, "color" : "#33CCCC",endX : 380, endY: 180, path : '/publications', text:"Publications"};
-                var skillsC= { "x": 0, "y": 0, "radius": 80, "color" : "#33CCCC", endX : 380, endY: 280, path : '/skills', text: "Skills"};
+                var aboutC = { "x": 0, "y": 0, "r": 80, "color" : "#33CCCC", path : '/about', text : "About"};
+                var publicationsC = { "x": 0, "y": 0, "r": 80, "color" : "#33CCCC", path : '/publications', text:"Publications"};
+                var skillsC= { "x": 0, "y": 0, "r": 80, "color" : "#33CCCC", path : '/skills', text: "Skills"};
 
                 var menuCircles = [aboutC, publicationsC, skillsC];
 
@@ -42,33 +39,34 @@ myapp.directive('fancyMenu', function(){
                     .enter()
                     .append('g')
                     .attr('id', "blahs")
-                    .each(function(d){
+                    .each(function(){
                         d3.select(this).append('circle')
                             .attr('id', 'myCircles')
                             .attr('cx', function(d){return d.x;})
                             .attr('cy', function(d){return d.y;})
-                            .attr('r', function(d){return d.radius;})
+                            .attr('r', function(d){return d.r;})
                              .style('visibility', 'hidden')
                              .attr('fill', function(d){return d.color;})
-                            .on('mouseover', function(d){
+                            .on('mouseover', function(){
                                 d3.select(this).transition()
                                     .attr('r', 100);
                             })
-                            .on('mouseout', function(d){
+                            .on('mouseout', function(){
                                 d3.select(this).transition()
                                     .duration(1000)
-                                    .attr('r', function(d){return d.radius; });
+                                    .attr('r', function(d){return d.r; });
                             })
                             .on('click', function(d){
                                 scope.changeView(d.path);
-                            })
+                            });
                            d3.select(this).append("text")
                             .text(function(d){return d.text}).style('visibility','hidden' )
                             .attr('id', "texts")
-                            .attr("x", function(d){return d.X ;})
-                            .attr("y", function(d){return d.Y ;})
-                            .attr('font-color', "#FF6600")
-                            .attr("font-size", '30px');
+                            .attr("font-color", "#FF6600")
+                            .attr("font-size", '30px')
+                            .on('click', function(d){
+                                   scope.changeView(d.path);
+                               });
 
 
                     });
@@ -76,21 +74,40 @@ myapp.directive('fancyMenu', function(){
                d3.select('#orangeSun')
                     .on('click', function(){
 
-                       console.log(d3.selectAll('#texts'));
-                       d3.selectAll('#myCircles').transition()
-                            .attr('cx', function(d){return d.endX;})
-                            .attr('cy', function(d){return d.endY;})
+                       var nofMenuItems = menuCircles.length;
+                       var rot = Math.PI /156;
+
+                       var data3 = make_circle(225, nofMenuItems, rot, 80);
+
+                       //changing data bound to elements
+                       d3.selectAll('#myCircles')
+                           .data(data3)
+                           .enter();
+
+                       d3.selectAll('#myCircles')
+                           .transition()
+                            .attr('cx', function(d){return d.cx;})
+                            .attr('cy', function(d){return d.cy;})
                             .style('visibility', 'visible')
                             .ease('elastic')
                             .duration(1000);
 
+                       d3.selectAll('#texts')
+                           .data(data3)
+                           .enter();
+
                        d3.selectAll('#texts').transition()
-                       .text(function(d){return d.text})
-                           .attr("x", function(d){return d.endX ;})
-                           .attr("y", function(d){return d.endY ;})
+                           .attr("x", function(d){return d.cx ;})
+                           .attr("y", function(d){return d.cy ;})
+                           .attr("dx", "-.85em").attr("dy", ".25em")
                            .style('visibility','visible' )
                            .ease('elastic')
-                           .duration(1000);;
+                           .duration(1000);
+
+                       //TODO avoid doing this
+                       d3.selectAll('#texts')
+                           .data(menuCircles)
+                           .enter();
 
                     });
 
@@ -110,7 +127,7 @@ myapp.directive('fancyMenu', function(){
                 var r6 = 410;
 
 
-                var size = 1;
+                //var size = 1;
                 var running;
 
                 var click = function()
@@ -125,7 +142,7 @@ myapp.directive('fancyMenu', function(){
                 d3.select('#svg').on('click', click);
 
 //draws the circle
-                var make_circle = function(r,n,offset)
+                var make_circle = function(r,n,offset, size)
                 {
                     var data = [];
                     for(var i = 0; i < n; i++)
@@ -172,9 +189,8 @@ myapp.directive('fancyMenu', function(){
 
                 var update_circles = function() {
                     rot += Math.PI /156;//decides the speed and direction (clockwise vs anticlockwise)of rotation
-
                     n = 90;
-                    var data6 = make_circle(r6, n, rot);
+                    var data6 = make_circle(r6, n, rot, 1);
                     step =  4;
                     var offset = -15;
                     circle_factory("six", data6, offset, step);
